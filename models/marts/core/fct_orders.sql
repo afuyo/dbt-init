bae7812b0a5237880390ade18f3b94442a29e518
Artur Mrozowski <armz@lundbeck.com>
2021-11-07 09:59:12 +0000

    bug fixing

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,14 +1,11 @@
-with orders as
-(
-    select order_id, customer_id from {{ref('stg_orders')}}
-),
+with
 payments as (
     select payments_id from {{ ref('stg_payments')}}
 ),
 final as (
     select orders.order_id, payments.payments_id, orders.customer_id 
-    from orders
-    left join orders using(order_id)
+    from {{ref('stg_orders')}}
+    left join payments using(order_id)
 
 )
 select * from final
