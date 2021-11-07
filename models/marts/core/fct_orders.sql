8fc6fcdf5789ad71611c0fd8406cd262bac6a1ac
Artur Mrozowski <armz@lundbeck.com>
2021-11-07 10:49:59 +0000

    fix fct

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -3,10 +3,10 @@ orders as (
     select * from {{ref('stg_orders')}}
 ),
 payments as (
-    select payment_id from {{ ref('stg_payments')}}
+    select payment_id,order_id from {{ ref('stg_payments')}}
 ),
 final as (
-    select orders.order_id, payments.payments_id, orders.customer_id 
+    select orders.orderid, payments.payments_id, orders.customer_id 
     from orders
     left join payments using(order_id)
 
