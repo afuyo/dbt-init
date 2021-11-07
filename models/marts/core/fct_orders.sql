73f3c8577216c0634795949a625484649121f4d6
Artur Mrozowski <armz@lundbeck.com>
2021-11-07 10:10:59 +0000

    fixing orders

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,10 +1,13 @@
 with
+orders as (
+    select * from {{ref('stg_orders')}}
+)
 payments as (
     select payment_id from {{ ref('stg_payments')}}
 ),
 final as (
     select orders.order_id, payments.payments_id, orders.customer_id 
-    from {{ref('stg_orders')}}
+    from orders
     left join payments using(order_id)
 
 )
