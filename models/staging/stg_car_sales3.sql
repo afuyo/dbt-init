4916cf7d3126083a5e6d8098adbf04c9a6339f2b
afuyo <artmro@gmail.com>
2022-09-15 11:29:10 +0200

    adding jaffle setup

--- models/staging/stg_car_sales3.sql
+++
@@ -1,8 +0,0 @@
-with car_sales3 as (
-    
-    select 
-        *
-    from demo_raw.json_raw_sch.car_sales3
-)
-
-select * from car_sales3
---
+++ models/staging/stg_car_sales3.txt
@@ -0,0 +1,8 @@
+with car_sales3 as (
+    
+    select 
+        *
+    from demo_raw.json_raw_sch.car_sales3
+)
+
+select * from car_sales3
